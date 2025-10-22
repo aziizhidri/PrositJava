3a985f578073feed24190aadbc211238fe9cf393
aziizhidri <mohamedaziz.hidri@esprit.tn>
2025-10-22 18:50:17 +0100

    prosit 2 - instr.7: créa b constructeur kther animaux w 7attinehom fil zoo

--- src/ZooManagement.java
+++ src/ZooManagement.java
@@ -1,19 +1,29 @@
 public class ZooManagement {
     public static void main(String[] args) {
-        // taw nkhallou les animaux s7i7in direct b constructeur
-        Animal lion = new Animal("felidae", "simba", 5, true);
-        Animal tiger = new Animal("felidae", "shere khan", 7, true);
-
-        // zoo b constructeur paramétré
+        // nzidou zoo wa7ed
         Zoo myZoo = new Zoo("wildlife park", "ariana", 25);
 
-        // n7ott les animaux fil tableau
+        // ncréiw b constructeur mra wa7da
+        Animal lion   = new Animal("felidae", "simba", 5, true);
+        Animal tiger  = new Animal("felidae", "shere khan", 7, true);
+        Animal zebra  = new Animal("equidae", "marty", 4, true);
+        Animal eagle  = new Animal("accipitridae", "aquila", 3, false);
+        Animal monkey = new Animal("cercopithecidae", "george", 2, true);
+
+        // n7ottouhom fil tableau (positions sahlîn)
         myZoo.animals[0] = lion;
         myZoo.animals[1] = tiger;
+        myZoo.animals[2] = zebra;
+        myZoo.animals[3] = eagle;
+        myZoo.animals[4] = monkey;
 
-        // affichage simple
-        System.out.println("zoo: " + myZoo.name + " fi " + myZoo.city);
-        System.out.println("1er animal: " + myZoo.animals[0].name);
-        System.out.println("2ème animal: " + myZoo.animals[1].name);
+        // affichage sgher bach nt2akked
+        System.out.println("zoo: " + myZoo.name + " fi " + myZoo.city + " (cages: " + myZoo.nbrCages + ")");
+        System.out.println("animals:");
+        System.out.println(" - " + myZoo.animals[0].name);
+        System.out.println(" - " + myZoo.animals[1].name);
+        System.out.println(" - " + myZoo.animals[2].name);
+        System.out.println(" - " + myZoo.animals[3].name);
+        System.out.println(" - " + myZoo.animals[4].name);
     }
 }
